a7143d4a032cf168392c3f92db0b602906833a42
dambrocioa <132759875+dambrocioa@users.noreply.github.com>
2023-12-05 19:47:38 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,2 @@
 import runpy
-runpy.run_module("src.interactive.pages.Intro", run_name="__main__", alter_sys=True)
+runpy.run_module("src.interactive.pages.5_💬_Sparky_Words", run_name="__main__", alter_sys=True)
