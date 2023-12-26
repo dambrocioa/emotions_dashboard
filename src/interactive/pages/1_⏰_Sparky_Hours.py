3ebe56a45b20dd6fa0e8cb8d395f94df1aa29ad0
dambrocioa <132759875+dambrocioa@users.noreply.github.com>
2023-12-25 18:32:10 -0600

    Update 1_⏰_Sparky_Hours.py

--- src/interactive/pages/1_⏰_Sparky_Hours.py
+++ src/interactive/pages/1_⏰_Sparky_Hours.py
@@ -30,18 +30,24 @@ users_names = {"user_kl_29":"Adrian Frydman"}
 
 @st.cache_data
 def get_data_hours(users_names):
-    conn = st.experimental_connection('s3', type=FilesConnection)
-    data_hours = conn.read("sparky-final-summaries/Emotions/SPARKY_HOURS/summary.csv", input_format="csv", ttl=600)
-    data_hours = data_hours.drop_duplicates()
-    
-    data_hours['date_obj'] = pandas.to_datetime(data_hours['date'],format='%Y-%m-%d %H:%M:%S')#.dt.time
-    data_hours["user_name"] = [users_names[user_key] for user_key in data_hours["user"]]
-    data_hours = data_hours[data_hours['user_name']!='UNKNOWN']
+    data_hours = pandas.DataFrame()
+    try:
+        conn = st.experimental_connection('s3', type=FilesConnection)
+        data_hours = conn.read("sparky-final-summaries/Emotions/SPARKY_HOURS/summary.csv", input_format="csv", ttl=600)
+        data_hours = data_hours.drop_duplicates()
+        
+        data_hours['date_obj'] = pandas.to_datetime(data_hours['date'],format='%Y-%m-%d %H:%M:%S')#.dt.time
+        data_hours["user_name"] = [users_names[user_key] for user_key in data_hours["user"]]
+        data_hours = data_hours[data_hours['user_name']!='UNKNOWN']
 
-    data_hours["humanized_week_info"] = ["Semana que inició en "+humanize.naturaldate(datetime.datetime.strptime(week_info,"%Y-%m-%d")) for week_info in data_hours["week_info"]]
-    data_hours["humanized_short_date"] = [humanize.naturaldate(datetime.datetime.strptime(short_date,"%Y-%m-%d")) for short_date in data_hours["short_date"]]
+        data_hours["humanized_week_info"] = ["Semana que inició en "+humanize.naturaldate(datetime.datetime.strptime(week_info,"%Y-%m-%d")) for week_info in data_hours["week_info"]]
+        data_hours["humanized_short_date"] = [humanize.naturaldate(datetime.datetime.strptime(short_date,"%Y-%m-%d")) for short_date in data_hours["short_date"]]
     
-    return data_hours
+        
+    except:
+        st.write("NO hay datos de chck in/ out")
+    finally:
+        return data_hours
 
 @st.cache_data
 def get_data_activity(users_names):
@@ -75,9 +81,9 @@ data_activity = get_data_activity(users_names)
 # The side bar that contains radio buttons for selection of charts-----------------------------------------------------------
 with st.sidebar:
     def update_selected_user():
-        st.session_state["selected_user"] = st.session_state['selected_user_hours']
+        st.session_state["selected_user"] = st.session_state['selected_user_activity']
     st.header('Persona')
-    users = tuple(list(data_hours["user"].unique()))
+    users = tuple(list(data_activity["user"].unique()))
     
     if 'selected_user' not in st.session_state:
         st.session_state["selected_user"] = users[0]
@@ -88,57 +94,100 @@ with st.sidebar:
             st.write('no user found')
             st.session_state['selected_user_hours'] = users[0]
 
-    selected_user_hours = st.radio('Selecciona una persona',(users),key="selected_user_hours",on_change=update_selected_user)
-    df = data_hours[data_hours['user']==selected_user_hours]
+    st.header('Persona')
+    selected_user = st.radio('Selecciona una persona',(users),key="selected_user_activity",on_change=update_selected_user)
+    
+    #---Activity Data
+    df_activity = data_activity[data_activity['user_name']==selected_user]
+    #df = data_hours[data_hours['user_name']==selected_user]
 
-
+    #---Hours Data
     st.header('Fecha')
-    dates = df['short_date'].unique()
+    dates = df_activity['short_start_time'].unique()
     selected_dates = st.multiselect('Selecciona una o varias fechas',dates, dates)
-    df = df[df['short_date'].isin(selected_dates)]
+    #df = df[df['short_date'].isin(selected_dates)]
 
 
     
 # The main window--------------------------
+annotated_text(tuple(["Esta información se actualiza cada 24 horas a la media noche.","","#fea"]))
+annotated_text(tuple(["Esto es, todos los días a la media noche se agrega a este panel la información generada por el usuario durante ese mismo día.","","#fea"]))
 
-#st.title("A Simple CO2 Emissions Dashboard")
-#st.write("an example of a Streamlit layout using a sidebar")
 
-df["time_str"] = df["date_obj"].dt.strftime('%H:%M:%S')
-df["fake_date"] = ['2020-01-08 '+item for item in df["time_str"]]
-df["fake_date_obj"] = pandas.to_datetime(df["fake_date"],format='%Y-%m-%d %H:%M:%S')
-df["time_truncated"] = [time_str[0:5] for time_str in df["time_str"]]
-n = df["short_date"].nunique()
+#HOURS DATA--------------
+
+if len(df)>0:
+    df["time_str"] = df["date_obj"].dt.strftime('%H:%M:%S')
+    df["fake_date"] = ['2020-01-08 '+item for item in df["time_str"]]
+    df["fake_date_obj"] = pandas.to_datetime(df["fake_date"],format='%Y-%m-%d %H:%M:%S')
+    df["time_truncated"] = [time_str[0:5] for time_str in df["time_str"]]
+    n = df["short_date"].nunique()
 
 
 with st.container():
-    st.header("Horas de Check in / Check out")
 
-    fig = px.scatter(df,
-        x="fake_date_obj",
-        y="short_date",
-        range_x=['00:00','23:59'],
-        color="action",
-        color_discrete_map={
-                            "check_in": "#16A085",
-                            "check_out": "#FA8072",
-                            "opt3": "#008aff"},
-        text="time_truncated")
+    #col1, col2 = st.columns(2)
+
+    #with col1:
+    #HOURS DATA--------------
+    if len()>0:
+        st.header("Horas de check in / check out")
+
+        fig = px.scatter(df,
+            x="fake_date_obj",
+            y="humanized_short_date",
+            range_x=['00:00','23:59'],
+            color="action",
+            color_discrete_map={
+                                "check_in": "#16A085",
+                                "check_out": "#FA8072",
+                                "opt3": "#008aff"},
+            text="time_truncated")
+        
+        fig.update_traces(marker_size=20,
+                            textposition='top center',
+                            hovertemplate = None,
+                            hoverinfo = "skip")
+        
+        fig.update_layout(
+            xaxis_title="Hora",
+            yaxis_title="Día")
+        
+        fig.update_layout(
+        yaxis_tickformat = '%Y-%m-%d',
+        xaxis_tickformat = '%H:%M',
+        showlegend=True)
+        fig.update_yaxes(nticks=n)
+        fig.update_layout(
+            xaxis_nticks=12)
+
+        st.plotly_chart(fig, theme=None, use_container_width=True)
+        
+        st.header("Actividad semanal en teclado")
+        st.markdown("Esta gráfica muestra el nivel de actividad en teclado. El 1 corresponde a un nivel de actividad continua equivalente a estar continuamente escribiendo en teclado y se muestra con el color más oscuro. El 0 corresponde a inactividad completa en teclado y mouse, y se muestra con el color más claro.")
     
-    fig.update_traces(marker_size=20,
-                        textposition='top center',
-                        hovertemplate = None,
-                        hoverinfo = "skip")
-    
+    #with col2:
+        #ACTIVITY DATA-----------
+    fig = go.Figure(data=go.Heatmap(
+            z=resampled_activity_data['actions'],
+            x=resampled_activity_data['dummy_date'],
+            y=resampled_activity_data['humanized_short_start_time'],
+            colorscale='Blues'))
+
     fig.update_layout(
-        xaxis_title="Hora",
-        yaxis_title="Día")
+        xaxis_nticks=12)
     
     fig.update_layout(
     yaxis_tickformat = '%Y-%m-%d',
     xaxis_tickformat = '%H:%M',
     showlegend=True)
-    fig.update_yaxes(nticks=n) 
 
-    st.plotly_chart(fig, theme=None, use_container_width=True) 
+    fig.update_layout(
+        xaxis_title="Hora",
+        yaxis_title="Día")
+
+    st.plotly_chart(fig, theme="streamlit")
+    #st.write(df_activity)
+
+    #st.write(resampled_activity_data)
 
